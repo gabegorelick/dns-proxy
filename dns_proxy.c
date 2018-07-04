490bdf6e76d6b5a84d44c8b3c4ccf2aaab48826e
Cookie Engineer <cookiengineer@protonmail.ch>
2018-07-04 09:11:12 +0200

    :rocket: Merge fixes by Brock Noland

--- dns_proxy.c
+++ dns_proxy.c
@@ -21,6 +21,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -46,6 +47,7 @@ char *USERNAME = "nobody";
 char *GROUPNAME = "nobody";
 int NUM_DNS = 0;
 int LOG = 0;
+int REWRITE_RESOLVECONF = 0;
 char **dns_servers;
 
 typedef struct {
@@ -71,47 +73,68 @@ char *get_value(char *line) {
 }
 
 char *string_value(char *value) {
+
 	char *tmp = (char*)malloc(strlen(value)+1);
 	strcpy(tmp, value);
 	value = tmp;
-	if (value[strlen(value)-1] == '\n')
+
+	if (value[strlen(value)-1] == '\n') {
 		value[strlen(value)-1] = '\0';
+	}
+
 	return value;
+
 }
 
 void parse_config(char *file) {
+
 	char line[80];
 
 	FILE *f = fopen(file, "r");
-	if (!f)
+	if (!f) {
 		error("[!] Error opening configuration file");
+	}
 
 	while (fgets(line, 80, f) != NULL) {
 		if (line[0] == '#')
 			continue;
 
-		if(strstr(line, "socks_port") != NULL) 
+		if (strstr(line, "socks_port") != NULL) {
 			SOCKS_PORT = strtol(get_value(line), NULL, 10);
-		else if(strstr(line, "socks_addr") != NULL)
+		} else if (strstr(line, "socks_addr") != NULL) {
 			SOCKS_ADDR = string_value(get_value(line));
-		else if(strstr(line, "listen_addr") != NULL)
+		} else if (strstr(line, "listen_addr") != NULL) {
 			LISTEN_ADDR = string_value(get_value(line));
-		else if(strstr(line, "listen_port") != NULL)
+		} else if (strstr(line, "listen_port") != NULL) {
 			LISTEN_PORT = strtol(get_value(line), NULL, 10);
-		else if(strstr(line, "set_user") != NULL)
+		} else if (strstr(line, "set_user") != NULL) {
 			USERNAME = string_value(get_value(line));
-		else if(strstr(line, "set_group") != NULL)
+		} else if (strstr(line, "set_group") != NULL) {
 			GROUPNAME = string_value(get_value(line));
-		else if(strstr(line, "resolv_conf") != NULL)
+		} else if (strstr(line, "rewrite_resolv_conf") != NULL) {
+
+			char *value = string_value(get_value(line));
+			for (int i = 0; value[i]; i++) {
+				value[i] = tolower(value[i]);
+			}
+			REWRITE_RESOLVECONF = strcmp(value, "true") == 0;
+
+		} else if (strstr(line, "resolv_conf") != NULL) {
 			RESOLVCONF = string_value(get_value(line));
-		else if(strstr(line, "log_file") != NULL)
+		} else if (strstr(line, "log_file") != NULL) {
 			LOGFILE = string_value(get_value(line));
+		}
+
 	}
-	if (fclose(f) != 0)
+
+	if (fclose(f) != 0) {
 		error("[!] Error closing configuration file");
+	}
+
 }
 
 void parse_resolv_conf() {
+
 	char ns[80];
 	int i = 0;
 	regex_t preg;
@@ -119,29 +142,38 @@ void parse_resolv_conf() {
 	regcomp(&preg, "^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+\n$", REG_EXTENDED);
 
 	FILE *f = fopen(RESOLVCONF, "r");
-	if (!f)
+	if (!f) {
 		error("[!] Error opening resolv.conf");
+	}
 
 	while (fgets(ns, 80, f) != NULL) {
 		if (!regexec(&preg, ns, 1, pmatch, 0))
 			NUM_DNS++;
 	}
 
-	if (fclose(f))
+	if (fclose(f)) {
 		error("[!] Error closing resolv.conf");
+	}
 
 	dns_servers = malloc(sizeof(char*) * NUM_DNS);
 
 	f = fopen(RESOLVCONF, "r");
 	while (fgets(ns, 80, f) != NULL) {
-		if (regexec(&preg, ns, 1, pmatch, 0) != 0)
+
+		if (regexec(&preg, ns, 1, pmatch, 0) != 0) {
 			continue;
+		}
+
 		dns_servers[i] = (char*)malloc(strlen(ns) + 1);
 		strcpy(dns_servers[i], ns);
 		i++;
+
 	}
-	if (fclose(f))
+
+	if (fclose(f)) {
 		error("[!] Error closing resolv.conf");
+	}
+
 }
 
 // handle children
@@ -150,6 +182,7 @@ void reaper_handle (int sig) {
 }
 
 void tcp_query(void *query, response *buffer, int len) {
+
 	int sock;
 	struct sockaddr_in socks_server;
 	char tmp[1024];
@@ -160,11 +193,13 @@ void tcp_query(void *query, response *buffer, int len) {
 	socks_server.sin_addr.s_addr = inet_addr(SOCKS_ADDR);
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock < 0) 
+	if (sock < 0) {
 		error("[!] Error creating TCP socket");
+	}
 
-	if (connect(sock, (struct sockaddr*)&socks_server, sizeof(socks_server)) < 0)
+	if (connect(sock, (struct sockaddr*)&socks_server, sizeof(socks_server)) < 0) {
 		error("[!] Error connecting to proxy");
+	}
 
 	// socks handshake
 	send(sock, "\x05\x01\x00", 3, 0);
@@ -173,12 +208,20 @@ void tcp_query(void *query, response *buffer, int len) {
 	srand(time(NULL));
 
 	// select random dns server
-	in_addr_t remote_dns = inet_addr(dns_servers[rand() % NUM_DNS]);
+	in_addr_t remote_dns;
+	if (NUM_DNS == 1) {
+		remote_dns = inet_addr(dns_servers[0]);
+	} else {
+		remote_dns = inet_addr(dns_servers[rand() % NUM_DNS]);
+	}
+
 	memcpy(tmp, "\x05\x01\x00\x01", 4);
 	memcpy(tmp + 4, &remote_dns, 4);
 	memcpy(tmp + 8, "\x00\x35", 2);
 
-	if (LOG == 1) { fprintf(LOG_FILE, "Using DNS server: %s\n", inet_ntoa(*(struct in_addr *)&remote_dns)); }
+	if (LOG == 1) {
+		fprintf(LOG_FILE, "Using DNS server: %s\n", inet_ntoa(*(struct in_addr *)&remote_dns));
+	}
 
 	send(sock, tmp, 10, 0);
 	recv(sock, tmp, 1024, 0);
@@ -186,9 +229,11 @@ void tcp_query(void *query, response *buffer, int len) {
 	// forward dns query
 	send(sock, query, len, 0);
 	buffer->length = recv(sock, buffer->buffer, 2048, 0);
+
 }
 
 int udp_listener() {
+
 	int sock;
 	char len, *query;
 	response *buffer = (response*)malloc(sizeof(response));
@@ -201,34 +246,55 @@ int udp_listener() {
 	dns_listener.sin_port = htons(LISTEN_PORT);
 	dns_listener.sin_addr.s_addr = inet_addr(LISTEN_ADDR);
 
-	// create our udp listener
+
+	// create UDP listener
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sock < 0)
+	if (sock < 0) {
 		error("[!] Error setting up dns proxy");
+	}
 
-	if(bind(sock, (struct sockaddr*)&dns_listener, sizeof(dns_listener)) < 0)
+	if (bind(sock, (struct sockaddr*)&dns_listener, sizeof(dns_listener)) < 0) {
 		error("[!] Error binding on dns proxy");
+	}
 
-	FILE *resolv = fopen("/etc/resolv.conf", "w");
-
-	if (!resolv)
-		error("[!] Error opening /etc/resolv.conf");
-
-	fprintf(resolv, "nameserver %s\n", LISTEN_ADDR);
-	fclose(resolv);
 
 	if (strcmp(LOGFILE, "/dev/null") != 0) {
+
 		LOG      = 1;
 		LOG_FILE = fopen(LOGFILE, "a+");
-		if (!LOG_FILE)
+		if (!LOG_FILE) {
 			error("[!] Error opening logfile.");
+		}
+
+		setvbuf(LOG_FILE, NULL, _IOLBF, 1024);
+		fprintf(LOG_FILE, "Starting up...\n");
+
 	}
+
+	if (REWRITE_RESOLVECONF) {
+
+		FILE *resolv = fopen("/etc/resolv.conf", "w");
+		if (!resolv) {
+			error("[!] Error opening /etc/resolv.conf");
+		}
+
+		fprintf(LOG_FILE, "Listening on %s ...\n", LISTEN_ADDR);
+		fprintf(resolv, "nameserver %s\n", LISTEN_ADDR);
+		fclose(resolv);
+
+	}
+
 
 	printf("[*] No errors, backgrounding process.\n");
 
 	// daemonize the process.
-	if(fork() != 0) { exit(0); }
-	if(fork() != 0) { exit(0); }
+	if (fork() != 0) {
+		exit(0);
+	}
+
+	if (fork() != 0) {
+		exit(0);
+	}
 
 	setuid(getpwnam(USERNAME)->pw_uid);
 	setgid(getgrnam(GROUPNAME)->gr_gid);
@@ -240,21 +306,33 @@ int udp_listener() {
 	reaper.sa_handler = reaper_handle;
 	sigaction(SIGCHLD, &reaper, 0);
 
-	while(1) {
+
+
+	while (1) {
+
 		// receive a dns request from the client
 		len = recvfrom(sock, buffer->buffer, 2048, 0, (struct sockaddr *)&dns_client, &dns_client_size);
 
 		// lets not fork if recvfrom was interrupted
-		if (len < 0 && errno == EINTR) { continue; }
-
-		// other invalid values from recvfrom
-		if (len < 0) {
-			if (LOG == 1) { fprintf(LOG_FILE, "recvfrom failed: %s\n", strerror(errno)); }
+		if (len < 0 && errno == EINTR) {
 			continue;
 		}
 
+		// other invalid values from recvfrom
+		if (len < 0) {
+
+			if (LOG == 1) {
+				fprintf(LOG_FILE, "recvfrom failed: %s\n", strerror(errno));
+			}
+
+			continue;
+
+		}
+
 		// fork so we can keep receiving requests
-		if (fork() != 0) { continue; }
+		if (fork() != 0) {
+			continue;
+		}
 
 		// the tcp query requires the length to precede the packet, so we put the length there
 		query = malloc(len + 3);
@@ -273,44 +351,54 @@ int udp_listener() {
 		free(query);
 
 		exit(0);
+
 	}
+
 }
 
 int main(int argc, char *argv[]) {
-	if (argc == 1)
+
+	if (argc == 1) {
+
 		parse_config("dns_proxy.conf");
-	else if (argc == 2) {
+
+	} else if (argc == 2) {
+
 		if (!strcmp(argv[1], "-h")) {
+
 			printf("Usage: %s [options]\n", argv[0]);
 			printf(" * With no parameters, the configuration file is read from 'dns_proxy.conf'.\n\n");
 			printf(" -n          -- No configuration file (socks: 127.0.0.1:9999, listener: 0.0.0.0:53).\n");
 			printf(" -h          -- Print this message and exit.\n");
 			printf(" config_file -- Read from specified configuration file.\n\n");
 			printf(" * The configuration file should contain any of the following options (and ignores lines that begin with '#'):\n");
-			printf("   * socks_addr  -- socks listener address\n");
-			printf("   * socks_port  -- socks listener port\n");
-			printf("   * listen_addr -- address for the dns proxy to listen on\n");
-			printf("   * listen_port -- port for the dns proxy to listen on (most cases 53)\n");
-			printf("   * set_user    -- username to drop to after binding\n");
-			printf("   * set_group   -- group to drop to after binding\n");
-			printf("   * resolv_conf -- location of resolv.conf to read from\n");
-			printf("   * log_file    -- location to log server IPs to. (only necessary for debugging)\n\n");
+			printf("   * socks_addr          -- socks listener address\n");
+			printf("   * socks_port          -- socks listener port\n");
+			printf("   * listen_addr         -- address for the dns proxy to listen on\n");
+			printf("   * listen_port         -- port for the dns proxy to listen on (most cases 53)\n");
+			printf("   * set_user            -- username to drop to after binding\n");
+			printf("   * set_group           -- group to drop to after binding\n");
+			printf("   * resolv_conf         -- location of resolv.conf to read from\n");
+			printf("   * rewrite_resolv_conf -- toggles whether or not to rewrite resolv.conf\n");
+			printf("   * log_file            -- location to log server IPs to. (only necessary for debugging)\n\n");
 			printf(" * Configuration directives should be of the format:\n");
 			printf("   option = value\n\n");
 			printf(" * Any non-specified options will be set to their defaults:\n");
-			printf("   * socks_addr   = 127.0.0.1\n");
-			printf("   * socks_port   = 9050\n");
-			printf("   * listen_addr  = 0.0.0.0\n");
-			printf("   * listen_port  = 53\n");
-			printf("   * set_user     = nobody\n");
-			printf("   * set_group    = nobody\n");
-			printf("   * resolv_conf  = resolv.conf\n");
-			printf("   * log_file     = /dev/null\n");
+			printf("   * socks_addr          = 127.0.0.1\n");
+			printf("   * socks_port          = 9050\n");
+			printf("   * listen_addr         = 0.0.0.0\n");
+			printf("   * listen_port         = 53\n");
+			printf("   * set_user            = nobody\n");
+			printf("   * set_group           = nobody\n");
+			printf("   * resolv_conf         = resolv.conf\n");
+			printf("   * rewrite_resolv_conf = false\n");
+			printf("   * log_file            = /dev/null\n");
 			exit(0);
-		}
-		else {
+
+		} else {
 			parse_config(argv[1]);
 		}
+
 	}
 
 	if (getuid() != 0) {
@@ -336,4 +424,5 @@ int main(int argc, char *argv[]) {
 	// start the dns proxy
 	udp_listener();
 	exit(EXIT_SUCCESS);
+
 }
